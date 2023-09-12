fdbc5e2ed9aac4c9b118d7a7ceabd7c3d8e84073
Kenneth Bogner <kenneth.bogner@testdouble.com>
2023-09-12 02:33:43 -0400

    Start faker and ex_machina before ex_unit

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,4 +1,4 @@
+Faker.start()
+{:ok, _} = Application.ensure_all_started(:ex_machina)
 ExUnit.start()
 Ecto.Adapters.SQL.Sandbox.mode(DerbyLive.Repo, :manual)
-{:ok, _} = Application.ensure_all_started(:ex_machina)
-Faker.start()
