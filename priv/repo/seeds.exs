7464f4225285c497e16e1793a8fc99cb3ef61ebd
Kenneth Bogner <kenneth.bogner@testdouble.com>
2023-09-12 21:29:56 -0400

    Add racer seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,15 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+for n <- 1..24 do
+  DerbyLive.Repo.insert!(%DerbyLive.Racing.Racer{
+    racer_id: n,
+    first_name: Faker.Name.first_name(),
+    last_name: Faker.Name.last_name(),
+    rank: Enum.random(["Lions", "Tigers", "Wolves", "Bears", "Webelos", "Arrow of Light"]),
+    group: "Cubs",
+    car_name: Faker.Company.name(),
+    car_number: n + 100
+  })
+end
